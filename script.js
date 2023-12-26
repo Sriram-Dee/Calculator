1d31eabbae54163e8de1a361b8904f65cc3cf081
Sriram-Dee <sriram14jeeva12@gmail.com>
2023-12-26 20:36:48 +0530

    Final

--- script.js
+++ script.js
@@ -2,8 +2,36 @@ let input= document.querySelector("input")
 let buttons = document.querySelectorAll('button')
 let output = ""
 
-buttons.onclick= () => {
-     ()=>{
-        console.log("hi") 
-    }
-}
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+
+        if(e.target.innerHTML == "="){
+            try{
+                output = eval(input.value).toString()
+                input.value= output
+            }
+
+            catch(error){
+                input.value = "Invalid"
+                output=""
+            }
+        }
+        else if(e.target.innerHTML == "AC"){
+            output=''
+            input.value= output
+        }
+        else if(e.target.innerHTML == "DEL"){
+            output = output.substring(0, output.length-1)
+            input.value= output
+        }
+        else{
+            input.value =""
+            output += e.target.innerHTML
+            input.value=output 
+        // console.log(input)
+        }
+
+    })
+})
+
+console.log()
