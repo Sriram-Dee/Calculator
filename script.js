fc1193f7842956878de0f6d816517e0f51621361
Sriram-Dee <sriram14jeeva12@gmail.com>
2023-12-26 12:50:00 +0530

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+let input= document.querySelector("input")
+let buttons = document.querySelectorAll('button')
+let output = ""
+
+buttons.onclick= () => {
+     ()=>{
+        console.log("hi") 
+    }
+}
